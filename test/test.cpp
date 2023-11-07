fce929a184f062d3ecdf71bb8cb4afbe1be00195
Yaossg <yaossg@outlook.com>
2023-11-07 21:08:37 +0800

    tests

---
+++ test/test.cpp
@@ -0,0 +1,34 @@
+#include "../runtime/common.hpp"
+#include "../common.hpp"
+#include "../runtime/interpretation.hpp"
+
+int main(int argc, const char* argv[]) {
+    Porkchop::forceUTF8();
+    const int argi = 2;
+    if (argc < argi) {
+        Porkchop::Error error;
+        error.with(Porkchop::ErrorMessage().fatal().text("too few arguments, input file expected"));
+        error.with(Porkchop::ErrorMessage().usage().text("PorkchopTest <input> [args...]"));
+        error.report(nullptr, true);
+        std::exit(10);
+    }
+    std::string input = argv[1];
+    std::string output = input.substr(0, input.find_last_of('.')) + ".o";
+    freopen(output.c_str(), "w", stdout);
+    std::string original = Porkchop::readText(input.c_str());
+    Porkchop::Source source;
+    Porkchop::tokenize(source, original);
+    Porkchop::Continuum continuum;
+    Porkchop::Compiler compiler(&continuum, std::move(source));
+    Porkchop::parse(compiler);
+    std::ignore = compiler.descriptor(); // for coverage
+    Porkchop::Interpretation interpretation(&continuum);
+    compiler.compile(&interpretation);
+    Porkchop::VM vm;
+    vm.init(argi, argc, argv);
+    auto ret = Porkchop::execute(&vm, &interpretation);
+    auto type = compiler.continuum->functions.back()->prototype()->R;
+    auto sf = Porkchop::stringifier(type);
+    fputs("Exited with returned object: ", stdout);
+    fputs(sf(ret).c_str(), stdout);
+}
